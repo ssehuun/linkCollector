5e161414c30ef05755ac3c98bb83e80aecb886d3
sehun <shn413@gmail.com>
2017-02-08 23:34:24 +0900

    add link

--- app.js
+++ app.js
@@ -15,31 +15,53 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  // var sql = 'SELECT id, address, description FROM link';
-  //
-  // conn.query(sql, function(err, topic, fields){
-  //
-  // });
-  res.render('index');
-});
+app.get('/index', function (req, res) {
+  var sql = 'SELECT id, title, description FROM link';
 
-app.post('/add', function(req, res){
-  var address = req.body.address;
-  var description = req.body.description;
-  var sql = 'INSERT INTO link (address, description) VALUES(?,?)';
-
-  conn.query(sql, [address, description], function(err, results, fields){
+  conn.query(sql, function(err, links, fields){
     if(err){
       console.log(err);
-      res.status(500).send('Internal Server error');
+      res.status(500).send('Internal Server Error');
     }else{
-      res.redirect('/');
+      res.render('index', {links:links});
     }
   });
 });
 
+app.get('/add', function(req, res){
+  var sql = 'SELECT id, title, description FROM link';
+  conn.query(sql, function(err, links, fields){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server error');
+    }else{
+      res.render('add', {links:links});
+    }
+  });
+});
 
+app.post('/add', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var sql = 'INSERT INTO link (title, description) VALUES(?,?)';
+
+  conn.query(sql, [title, description], function(err, links, fields){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server error');
+    }else{
+      res.redirect('/add');
+    }
+  });
+});
+
+app.get('/delete', function(req, res){
+  
+  var sql = 'DELETE from link WHERE id=?';
+  conn.query(sql, [title], function(err, links, fields){
+
+  });
+});
 //404 error handling
 app.use(function (req, res, next) {
   var error = '<h1>FUCK! 404 Error!</h1>';
